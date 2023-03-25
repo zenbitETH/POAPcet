0a88f941240fc863af7ce6f73b65387195f58d61
habacuc.eth <habacuc.vera@gmail.com>
2023-03-25 15:22:33 -0600

    Router for manage page

--- vite-app/src/components/Header.tsx
+++ vite-app/src/components/Header.tsx
@@ -1,30 +1,37 @@
-import { ConnectButton } from "@rainbow-me/rainbowkit";
-import { useAccount } from "wagmi";
-import Logo from "../Public/banner.png"
-
+import { ConnectButton } from '@rainbow-me/rainbowkit';
+import { useAccount } from 'wagmi';
+import Logo from '../Public/banner.png';
+import { useNavigate } from 'react-router-dom';
 
 export function Header() {
+  const { isConnected } = useAccount();
+  const navigate = useNavigate();
 
-const { isConnected } = useAccount();
+  const goToManageFaucet = () => {
+    navigate('/manage');
+  };
+  
 
-    return (
-        <div className="fixed top-0 py-3 w-full flex justify-between items-center">
-            <img className="mx-5" width={250} src={Logo} alt=""/>
-            <div className="mx-5 flex gap-10">
-                {isConnected && (
-                    <div className="
-                        px-5 py-2 bg-red-500/50 rounded-2xl 
-                        text-2xl font-bold text-white 
-                        hover:text-black hover:bg-red-500 cursor-pointer 
-                        hover:scale-105 hover:drop-shadow-lg transition-all 
-                        duration-200 ease-in-out">
-                            Manage
-                    </div>
-                )}
-                    
-                    <ConnectButton />
-                </div>
-                
-        </div>
-    )
+  return (
+    <div className="fixed top-0 py-3 w-full flex justify-between items-center">
+      <a href='/'><img className="mx-5" width={250} src={Logo} alt="" /></a>
+      <div className="mx-5 flex gap-10">
+        {isConnected && (
+          <div
+            onClick={goToManageFaucet}
+            className="
+              px-5 py-2 bg-red-500/50 rounded-2xl
+              text-2xl font-bold text-white
+              hover:text-black hover:bg-red-500 cursor-pointer
+              hover:scale-105 hover:drop-shadow-lg transition-all
+              duration-200 ease-in-out"
+          >
+            Manage
+          </div>
+        )}
+
+        <ConnectButton />
+      </div>
+    </div>
+  );
 }
--- vite-app/src/main.tsx
+++ vite-app/src/main.tsx
@@ -5,22 +5,22 @@ import * as ReactDOM from "react-dom/client";
 import { WagmiConfig } from "wagmi";
 import '../src/Styles/index.css'
 import { Header } from "./components";
-
-import { App } from "./App";
+import ManageFaucet from "./pages/manage";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { chains, client } from "./wagmi";
+import { App } from "./App";
 
-/**
- * Root providers and initialization of app
- * @see https://reactjs.org/docs/strict-mode.html
- * @see https://wagmi.sh/react/WagmiConfig
- * @see https://www.rainbowkit.com/docs/installation
- */
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <WagmiConfig client={client}>
       <RainbowKitProvider chains={chains}>
-        <Header/>
-        <App />
+        <Router>
+          <Header />
+          <Routes>
+            <Route path="/" element={<App />} />
+            <Route path="/manage" element={<ManageFaucet />} />
+          </Routes>
+        </Router>
       </RainbowKitProvider>
     </WagmiConfig>
   </React.StrictMode>,
