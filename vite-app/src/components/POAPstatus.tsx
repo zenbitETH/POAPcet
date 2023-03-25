304b3b9693bf5e26d8bb7d55911d573cd2170a78
habacuc.eth <habacuc.vera@gmail.com>
2023-03-25 12:15:23 -0600

    Claim button

--- vite-app/src/components/POAPstatus.tsx
+++ vite-app/src/components/POAPstatus.tsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 const POAPStatus: React.FC = () => {
   const [isPOAPHolder, setIsPOAPHolder] = useState<boolean>(true);
 
-  // Example: Toggle the POAP holder status when the component is clicked
+  // Toggle const should be substituted with the POAP api call 
   const togglePOAPHolder = () => {
     setIsPOAPHolder(!isPOAPHolder);
   };
@@ -11,8 +11,9 @@ const POAPStatus: React.FC = () => {
   return (
     <div className="text-center" onClick={togglePOAPHolder}>
       {isPOAPHolder ? (
-        <div className="text-green-600">
-          You are holding the POAP, claim your test funds!
+        <div className='border border-green-500 rounded-2xl max-w-2xl mx-auto h-96 grid items-center'>
+          <div className="text-green-600">You are holding the POAP, claim your test funds!</div>
+          <button className='bg-gray-200 rounded-2xl hover:bg-green-500 hover:text-white py-2 px-4 mx-auto'> Claim test funds</button>
         </div>
       ) : (
         <div className="text-red-600">
